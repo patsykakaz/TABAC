15adc004575785ac1c70810ed1a3075a8449d286
patsykakaz <philippe@lesidecar.fr>
2016-05-01 18:14:07 +0200

    models.py 101

--- MAIN/admin.py
+++ MAIN/admin.py
@@ -9,6 +9,9 @@ from mezzanine.blog.admin import BlogPostAdmin
 from mezzanine.blog.models import BlogPost
 
 
+class BrandAdmin(PageAdmin):
+    fieldsets = deepcopy(PageAdmin.fieldsets)
+
 class TopicAdmin(PageAdmin):
     fieldsets = deepcopy(PageAdmin.fieldsets)
 
@@ -17,6 +20,8 @@ class JobInline(admin.TabularInline):
     extra = 1
 
 company_fieldsets = deepcopy(PageAdmin.fieldsets)
+company_fieldsets[0][1]["fields"].insert(-1, "brands")
+company_fieldsets[0][1]["fields"].insert(-1, "topic")
 company_fieldsets[0][1]["fields"].insert(-1, "adress")
 company_fieldsets[0][1]["fields"].insert(-1, "zipCode")
 company_fieldsets[0][1]["fields"].insert(-1, "area")
@@ -24,7 +29,6 @@ company_fieldsets[0][1]["fields"].insert(-1, "city")
 company_fieldsets[0][1]["fields"].insert(-1, "country")
 company_fieldsets[0][1]["fields"].insert(-1, "email")
 company_fieldsets[0][1]["fields"].insert(-1, "tel")
-company_fieldsets[0][1]["fields"].insert(-1, "topic")
 
 class CompanyAdmin(PageAdmin):
     inlines = (JobInline,)
@@ -34,10 +38,12 @@ class CompanyAdmin(PageAdmin):
 person_fieldsets = deepcopy(PageAdmin.fieldsets)
 person_fieldsets[0][1]["fields"].insert(-1, "email")
 person_fieldsets[0][1]["fields"].insert(-1, "tel")
+person_fieldsets[0][1]["fields"].insert(-1, "highlight")
 class PersonAdmin(PageAdmin):
     inlines = (JobInline,)
     fieldsets = person_fieldsets
 
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Company, CompanyAdmin)
--- MAIN/models.py
+++ MAIN/models.py
@@ -12,18 +12,24 @@ from mezzanine.core.models import RichText
 from mezzanine.core.fields import RichTextField, FileField
 from mezzanine.utils.models import upload_to
 
-# bobby du mois
-# société du mois
 
-# class Marque():
+class Brand(Page):
+    def save(self, *args, **kwargs):
+        self.in_menus = []
+        if not self.parent:
+            self.parent = Page.objects.get(title='MARQUES')
+        super(Marque, self).save(*args, **kwargs)
 
 class Topic(Page):
     def save(self, *args, **kwargs):
         self.in_menus = []
+        if not self.parent:
+            self.parent = Page.objects.get(title='RUBRIQUES')
         super(Topic, self).save(*args, **kwargs)
 
 class Company(Page):
-    topic = models.ManyToManyField('Topic')
+    topic = models.ManyToManyField('Topic',blank=True)
+    brands = models.ManyToManyField('Brand',blank=True)
     adress = models.CharField(max_length=255, null=False,blank=False)
     zipCode = models.CharField(max_length=255, null=False,blank=False)
     area = models.CharField(max_length=255, null=False,blank=True)
@@ -31,21 +37,29 @@ class Company(Page):
     country = models.CharField(max_length=255,null=False,blank=False)
     email = models.EmailField(null=False,blank=True)
     tel = models.CharField(max_length=20, null=False,blank=True)
+    highlight = models.BooleanField(default=False,null=False,blank=True)
 
     def save(self, *args, **kwargs):
         self.in_menus = []
+        # if not self.area:
+            # if not self.country or self.country.lower() == "france":
+                # deduce area from zipCode[:2]
         super(Company, self).save(*args, **kwargs)
 
 class Person(Page):
-    # BOBBY DU MOIS
     companies = models.ManyToManyField(Company,through='Job')
     email = models.EmailField(null=False,blank=True)
     tel = models.CharField(max_length=20, null=False,blank=True)
     # adresse (full + département)
+    highlight = models.BooleanField(default=False,null=False,blank=True)
 
     def save(self, *args, **kwargs):
+        print('*********************************')
+        print(str(self.title) + ' is BEING SAVED')
+        print('*********************************')
         self.in_menus = []
-        super(Person, self).save(*args, **kwargs)
+        self.parent = Page.objects.get(title='MEMBRES')
+        super(Person, self).save(*args,**kwargs)
 
 class Job(models.Model):
     person = models.ForeignKey(Person)
